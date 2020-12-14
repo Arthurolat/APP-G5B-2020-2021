462a62c203b2b3a5086ca5c2c37da1116d51d8f1
Clara Augie <claraaugie92@gmail.com>
2020-12-14 14:22:56 +0100

    MAJ

--- php/admin-lancer-test1_connu.php
+++ php/admin-lancer-test1_connu.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="../css/uti-accueil_admin1.css" />
+    <link rel="stylesheet" href="../css/gestionnaire_lancer-test1_connu.css" />
     <link rel="stylesheet" href="../css/header.css" />
     <link rel="stylesheet" href="../css/footer.css" />
     <title>Page d'accueil</title>
@@ -51,56 +51,49 @@
             </div>
 
             <div id=contenu>
-                <h2> Lancement d'une session de tests </h2>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
+            <h2> Lancement d'une session de tests </h2>
 
+            <p>Pour lancer une session de tests, vous devez commencer par attribuer la session à un utilisateur.</p>
+
+            <div class="step_progress">
+                <ul>
+                    <li class="ok">Démarrage session</li>
+                    <li>Choisir le boitier</li>
+                    <li>Phase tests</li>
+                    <li>Validation</li>
+                </ul>
+            </div>
+
+            <div class=onglet>
+                <div class=element1>
+                    <p>Utilisateur déjà existant</p>
+                </div>
+                <div class=element2>
+                    <p><a href="gestionnaire_lancer-test1_inconnu.html">Créer un nouveau compte utilisateur</a></p>
+                </div>
+            </div>
+            <div class=formulaire>
+
+                <form method="post" action="gestionnaire_lancer-test2.html">
+                    <p>
+                        <fieldset>
+                            <p><label>Nom : <input type="text" name="nom" size="50" placeholder="Le nom de l'utilisateur..."></label></p>
+                            <p><label>Prénom : <input type="text" name="prenom" size="50" placeholder="Le prénom de l'utilisateur..."></label></p>
+                            <p><label>Date et heure : <input type="datetime-local" name="date"></label></p>
+                            <p class="bouton"><input type="submit" value="Suivant" /></p>
+                        </fieldset>
+                    </p>
+                </form>
+
+                <!-- <p>
+                    <a href="uti-gestionnaire_lancer-test2.html">Suivant</a>
                 </p>
+                -->
 
             </div>
+            </br>
+
+        </div>
         </section>
     </div>
 
