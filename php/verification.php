787788ac56e38a1ff27a78e7f52d4491c608d690
Clara Augie <claraaugie92@gmail.com>
2020-12-15 19:09:57 +0100

    fonction connexion ok

--- php/verification.php
+++ php/verification.php
@@ -9,54 +9,57 @@ if(isset($_POST['mail']) && isset($_POST['mdp']))
    $db_nom     = 'appg5b_bdd';
    $db_host     = 'mysql-appg5b.alwaysdata.net';
    $db = mysqli_connect($db_host, $db_id, $db_mdp,$db_nom)
-  or die('could not connect to database');
+   or die('could not connect to database');
 
 
     // on applique les deux fonctions mysqli_real_escape_string et htmlspecialchars
     // pour éliminer toute attaque de type injection SQL et XSS
-    $mail = mysqli_real_escape_string($db,htmlspecialchars($_POST['mail'])); 
-    $mdp = mysqli_real_escape_string($db,htmlspecialchars($_POST['mdp']));
+   $mail = mysqli_real_escape_string($db,htmlspecialchars($_POST['mail'])); 
+   $mdp = mysqli_real_escape_string($db,htmlspecialchars($_POST['mdp']));
     
-   
-    
-        $requete = "SELECT count(*) FROM personne where 
-              mail = '".$mail."' and mdp = '".$mdp."' ";
-         // select mail, mdp, statut from personne
-         // if statut=='utilisateur' alors connect interface uti 
-         // idem pour gestionnaire et admin
-        $exec_requete = mysqli_query($db,$requete);
-        $reponse      = mysqli_fetch_array($exec_requete);
-        $count = $reponse['count(*)'];
-        if($count!=0) // nom d'utilisateur et mot de passe correctes
-        {
+   if($mail !== "" && $mdp !== "")
+   {
+      $requete = "SELECT count(*), statut FROM personne where 
+            mail = '".$mail."' and mdp = '".$mdp."' ";
+      // select mail, mdp, statut from personne
+      // if statut=='utilisateur' alors connect interface uti 
+      // idem pour gestionnaire et admin
+      $exec_requete = mysqli_query($db,$requete);
+      $reponse      = mysqli_fetch_array($exec_requete);
+      $count = $reponse['count(*)'];
+      $statut= $reponse['statut'];
+      if($count!=0) // nom d'utilisateur et mot de passe correctes
+      {
 
-           $_SESSION['mail'] = $mail;
-           if($mail=='administrateur@mail.com')
-           {
-            header('Location: admin-lancer-test1_connu.php');
-           }
-           if($mail=='gestionnaire@mail.com')
-           {
-            header('Location: gestionnaire_lancer-test1_connu.php');
-           }
-           if($mail=='utilisateur@mail.com')
-           {
-            header('Location: uti-accueil_resultat_date.php');
-           }
-           
-        }
-        else
-        {
+         $_SESSION['mail'] = $mail;
+         if($statut=='administrateur')
+         {
+         header('Location: admin-lancer-test1_connu.php');
+         }
+         if($statut=='gestionnaire')
+         {
+         header('Location: gestionnaire_lancer-test1_connu.php');
+         }
+         if($statut=='utilisateur')
+         {
+         header('Location: uti-accueil_resultat_date.php');
+         }
+         
+      }
+      else
+      {
+         echo "erreur : vérifier le mot de passe et/ou l'identifiant"; // utilisateur ou mot de passe incorrect
+      }
+   }
+   else
+   {
+      echo "erreur : veuiller remplir le mot de passe et/ou l'identifiant"; // utilisateur ou mot de passe vide
+   }
 
-           echo "erreur not match"; // utilisateur ou mot de passe incorrect
-
-        }
-    }
-    else
-    {
-
-
-       echo "erreur not exist"; // utilisateur ou mot de passe vide
-
-    }
-
+}
+else
+{
+   header('Location: page_connexion.php');
+}
+mysqli_close($db); // fermer la connexion
+?>
