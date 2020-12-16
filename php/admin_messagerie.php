1ae6236db113952463647798c744fd42d4035b26
Arthurolat <arthur.latourrette@gmail.com>
2020-12-16 12:17:15 +0100

    modif header

--- php/admin_messagerie.php
+++ php/admin_messagerie.php
@@ -14,7 +14,7 @@
 
 <body>
 
-    <?php include("header.php"); ?>
+    <?php include("header2.php"); ?>
 
 <!-------messagerie---------------------->
 
---
+++ php/header2.php
@@ -0,0 +1,22 @@
+<header>
+    <nav>
+        <ul>
+            <div id="logoinfi_container">
+                <img class="logo_infinite" src="../images/logo_infinite.gif" alt="" />
+            </div>
+
+            <div id="logosolution_container">
+                <img class="logo_solution" src="../images/logo_solution.png" alt="" />
+            </div>
+
+            <div id="home">  
+                <li>
+                    <a href="accueil.php"><img class="logohome" src="../images/icones/logo_home.png" /></a>
+                </li>
+                <div id="deconnexion">
+                    <a href="accueil.php">Déconnexion</a>    
+                </div>
+            </div>
+        </ul>  
+    </nav>
+</header>
