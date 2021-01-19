58fd96ec16671f8f145c5df7a3600346676b28f8
Clara Augie <claraaugie92@gmail.com>
2021-01-19 19:37:27 +0100

    correction barre de nav

--- modele/test.php
+++ modele/test.php
@@ -1,7 +1,8 @@
 <?php
 
-$bdd = new PDO('mysql:host=mysql-appg5b.alwaysdata.net;dbname=appg5b_bdd;charset=utf8', 'appg5b', '@ppg5b2020');
-
+//$bdd = new PDO('mysql:host=mysql-appg5b.alwaysdata.net;dbname=appg5b_bdd;charset=utf8', 'appg5b', '@ppg5b2020');
+$bdd = new PDO('mysql:host=localhost;dbname=musique;charset=utf8', 'root', 'mdp_root');
+/*
 $prenom= 'clara'; 
 $nom='augié';
 
@@ -10,6 +11,16 @@ foreach  ($bdd->query($sql) as $row){
     $idacteur=$row['idacteur']; 
 }
 echo $idacteur; 
+*/
+$sql = ("SELECT * from concert");
+
+
+foreach  ($bdd->query($sql) as $row){
+    $id=$row['id_Concert'].' '.$row['nomConcert']; 
+}
+echo $id;
+
+// https://www.youtube.com/watch?v=arjtn3uNiK4
 
 ?>
 
