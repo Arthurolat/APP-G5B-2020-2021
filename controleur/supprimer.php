8a8dd7b6a8d7956567628672e4daa6f28ae2e528
Thomas <thomas.ledeventec1@gmail.com>
2021-01-22 10:49:29 +0100

    supprimer

--- controleur/supprimer.php
+++ controleur/supprimer.php
@@ -1,21 +1,15 @@
-<?php
-case isset($_GET['action']) && ($_GET['action'] == 'delete'):
-	if($_GET['validationWait'] == true) {
-		echo '<form action="supprimer.php" method="post");">';
-		echo '<span>Etes-vous sûr de vouloir supprimer cet utilisateur ?</span>';
-		echo '<input type="hidden" name="id" value="'. $_GET['id'] .'" />';
-		echo '<input type="hidden" name="action" value="delete" />';
-		echo '<input type="submit" name="yes" value="Yes" />';
-		echo '<input type="submit" name="no" value="No" />';
-		echo '</form>';
-	}
+<?php 
 
-	break;
-	
-case isset($_POST['yes']):
-	$_GET['validationWait'] = false;
-	$c = new Client();
-	$c->delete($_POST['id']);
-	break;
+$bdd = new PDO('mysql:host=mysql-appg5b.alwaysdata.net;dbname=appg5b_bdd;charset=utf8', 'appg5b', '@ppg5b2020');
 
-	?>
+$pdoStat = $bdd ->prepare('DELETE FROM personne WHERE idacteur=:num LIMIT 1');
+
+$pdoStat-> bindValue(':num',$_GET['numPersonne'], PDO :: PARAM_INT);
+
+$executeIsOk = $pdoStat->execute();
+
+
+
+
+
+?>
